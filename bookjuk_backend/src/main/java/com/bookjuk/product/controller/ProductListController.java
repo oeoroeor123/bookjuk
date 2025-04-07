3cdb86170d2439909fbf307fc524c4c299619abd
oeoroeor123 <oeoroeor123@naver.com>
2025-04-07 20:15:25 +0900

    상품 컨트롤러 오타 및 간격 수정

--- bookjuk_backend/src/main/java/com/bookjuk/product/controller/ProductListController.java
+++ bookjuk_backend/src/main/java/com/bookjuk/product/controller/ProductListController.java
@@ -31,58 +31,59 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "상품", description = "상품 API")
 public class ProductListController {
 
-	private final ProductListService productListService;
+  private final ProductListService productListService;
 	
 	
-	// 신규 상품 리스트 조회
-	@Operation(summary = "신규 상품 목록 조회", description = "신규 상품 목록 조회")
-	@GetMapping(value = "/product/new", produces = "application/json")
-	public ResponseMessage ProductList(
-			@RequestParam(required = false) LocalDate startDate,
-			@RequestParam(required = false) LocalDate endDate,
-			@RequestParam(required = false) Integer genreId,
-			Pageable pageable) {
-		return ResponseMessage.builder()
-				             .status(200)
-				             .message("신규 상품 목록 조회 성공")
-				             .results(productListService.findProductList(startDate, endDate, genreId, pageable))
-				             .build();
-	}
+  // 신규 상품 리스트 조회
+  @Operation(summary = "신규 상품 목록 조회", description = "신규 상품 목록 조회")
+  @GetMapping(value = "/product/new", produces = "application/json")
+  public ResponseMessage ProductList(
+        @RequestParam(required = false) LocalDate startDate,
+		@RequestParam(required = false) LocalDate endDate,
+		@RequestParam(required = false) Integer genreId,
+		Pageable pageable) {
+	return ResponseMessage.builder()
+			             .status(200)
+			             .message("신규 상품 목록 조회 성공")
+			             .results(productListService.findProductList(startDate, endDate, genreId, pageable))
+			             .build();
+  }
 	
 	
-	// 상품 상세 + 상품 문의글 + 상품 리뷰 글 조회	
-	@Operation(summary = "상품 상세, 문의글, 리뷰글 조회", description = "상품 상세, 문의글, 리뷰글 조회")
-	@GetMapping(value="/product/detail/{productId}", produces = "application/json")
-	public ResponseMessage Productdetail(@PathVariable(name = "productId") Integer productId) {
+  // 상품 상세 + 상품 문의글 + 상품 리뷰 글 조회	
+  @Operation(summary = "상품 상세, 문의글, 리뷰글 조회", description = "상품 상세, 문의글, 리뷰글 조회")
+  @GetMapping(value="/product/detail/{productId}", produces = "application/json")
+  public ResponseMessage Productdetail(@PathVariable(name = "productId") Integer productId) {
 		
-		// 서비스에서 map으로 한번에 가져오기 
-		Map<String, Object> productDetails = productListService.findProductById(productId);
+    // 서비스에서 map으로 한번에 가져오기 
+	Map<String, Object> productDetails = productListService.findProductById(productId);
 		
-		// 상품과 문의글, 리뷰을 각각 분리
-		ProductDto product = (ProductDto) productDetails.get("product");
-		List<ProductInqDto> inquiries = (List<ProductInqDto>) productDetails.get("inquiries");
-		List<ProductReviewDto> reviews = (List<ProductReviewDto>) productDetails.get("reviews");
+	// 상품과 문의글, 리뷰을 각각 분리
+	ProductDto product = (ProductDto) productDetails.get("product");
+	List<ProductInqDto> inquiries = (List<ProductInqDto>) productDetails.get("inquiries");
+	List<ProductReviewDto> reviews = (List<ProductReviewDto>) productDetails.get("reviews");
 		
-		if (product == null) {
-			product = new ProductDto();
-		}
+	  if (product == null) {
+     	  product = new ProductDto();
+      }
 		
-		if (inquiries == null) {
-			inquiries = new ArrayList<>();
-		}
+	  if (inquiries == null) {
+		  inquiries = new ArrayList<>();
+	  }
 		
-		if (reviews == null) {
-			reviews = new ArrayList<>();
-		}
+	  if (reviews == null) {
+		  reviews = new ArrayList<>();
+	  }
 		
-		return ResponseMessage.builder()
-				     .status(200)
-				     .message("상품 상세 조회 성공")
-				     .results(Map.of("product", product, "inquiries", inquiries, "reviews", reviews))
-				     .build();
-	}
-	
-  @Operation(summary = "상품 문의 상세 조회", description = "상품문의 상세를 확인하는 기능입니다.")
+	  return ResponseMessage.builder()
+				    .status(200)
+				    .message("상품 상세 조회 성공")
+				    .results(Map.of("product", product, "inquiries", inquiries, "reviews", reviews))
+				    .build();
+	 }
+
+  // 상품 문의 상세 조회
+  @Operation(summary = "상품 문의 상세 조회", description = "상품 문의 상세 조회")
   @GetMapping(value = "/product/inquiry/{inquiryId}", produces = "application/json")
   public ResponseMessage getInquiry(@PathVariable(name = "inquiryId") Integer inquiryId) {
     return ResponseMessage.builder()
